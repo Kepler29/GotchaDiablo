056a1746b615e795f73e27969e71898e3941d61d
Kepler Reyes Hernandez <keplerreyeshern@gmail.com>
2023-01-05 12:39:59 -0600

    Update 1.1

--- page/src/environments/environment.prod.ts
+++ page/src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   appVersion: 'v3.0.2',
   USERDATA_KEY: 'authb04l10b25',
-  apiUrl: 'http://backend.dev.gotchadiablo.com/api',
-  adminUrl: 'http://admin.dev.gotchadiablo.com/',
-  pageUrl: 'http://dev.gotchadiablo.com/',
+  apiUrl: 'https://backend.gotchadiablo.com/api',
+  adminUrl: 'https://admin.gotchadiablo.com/',
+  pageUrl: 'https://gotchadiablo.com/',
 };
